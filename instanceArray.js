4dd552285d260d744fd3d53f99174b3d9fba10b1
Scott Harvey <scottyharvey33@gmil.com>
2016-05-18 21:46:15 -0600

    Couple Left

--- instanceArray.js
+++ instanceArray.js
@@ -1,5 +1,5 @@
 /*
-  Your 3 Users will be the following. 
+  Your 3 Users will be the following.
     0) Tyler, tylermcginnis33@gmail.com, 'iLoveJS'
     1) Cahlan, cahlan@devmounta.in, 'iLoveHashtags'
     2) Lenny, lenny@theLenster.com, 'iLoveLentilSoup'
@@ -13,29 +13,29 @@ var User = function(name, email, pw){
 
 //Create an Array called 'users' that will store all our instances of User.
 
-  //code here
+var users = [this.name, this.email, this.pw, this.gender];
 
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
-  //code here
+  users.prototype.push("Tyler", "tylermcginnis33@gmail.com", 'iLoveJS', 'male');
 
 console.log('Tyler\'s information is ');
 //Console.log all of Tylers information
 
-  //code here
+console.log(user[0]);
 
 console.log('Lenny\'s information is ');
 //Now console.log all of Lennys information
 
-  //code here
+console.log(user[1]);
 
 
 //Now create another instance of User using your own information and then add that to your users array.
 
-  //code here
+console.log(user[2]);
 
 console.log('All my users names are ');
-//Now loop through your users Array and console.log every users name. 
+//Now loop through your users Array and console.log every users name.
 
   //code here
